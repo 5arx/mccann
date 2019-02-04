5ff32fdc5fdef64038f9d8523995876a96f8ae8e
indra <dev@indrasarkar.com>
2019-02-04 10:34:44 +0000

    Set-up projects

---
+++ Romanizer/Romanizer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RomanizerLib;
+
+namespace RomanizerDemo
+{
+    class Program
+    {
+        private static readonly Romanizer _romanizer = new Romanizer();
+
+        static void Main(string[] args)
+        {
+            for (int i=1; i< 4000; i++)
+            {
+                Console.WriteLine($"{i} = {_romanizer.Translate(i)}");
+            }
+
+            
+        }
+    }
+}
---
+++ Romanizer/RomanizerLib/Romanizer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RomanizerLib
+{
+    public class Romanizer
+    {
+        public string Translate(int i)
+        {
+            return i;
+        }
+    }
+}
